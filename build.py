a613bc53f7619aabf045310456ed4d0f9ab384d7
UnknownSeal <sebbe981214@gmail.com>
2021-08-23 21:34:46 +0200

    Support for Random and Random end

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ PLATFORM_TOOLSET = 'v140'
 F4SE_REVISION    = 'tags/v0.6.17'
 
 # Run build tools
-buildOK = os.system('python tools/build-tools/build_plugin.py "{}" "{}" "{}" "{}"'
+buildOK = os.system('tools/build-tools/build_plugin.py "{}" "{}" "{}" "{}"'
     .format(BUILD_DIR, PROJECT_DIR, PLATFORM_TOOLSET, F4SE_REVISION))
     
 # Report result
